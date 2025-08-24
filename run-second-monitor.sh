489f5e4391fe47cb9c8baa18140dc0c24a2ae73d
rexsybimatw <rexsy.bimq12@gmail.com>
2025-08-24 13:14:13 +0700

    major update on .tmux.conf

--- run-second-monitor.sh
+++ run-second-monitor.sh
@@ -2,7 +2,6 @@
 
 LOG_FILE="/home/rexsybimatw/tablet-log.txt"
 echo "Xiaomi tablet connected on $(/usr/bin/date)" >> "$LOG_FILE"
-
 /usr/bin/adb start-server  # Ensure ADB is running
 /usr/bin/adb reverse tcp:5900 tcp:5900 >> "$LOG_FILE" 2>&1
 echo "reverse tcp:5900 success" >> "$LOG_FILE"
